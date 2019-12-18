8e4f720cec1cb386bc6251c1b55ae7a7f2de4d00
[jose] <jose_sera_sera@hotmail.com>
2019-12-18 17:18:15 +0100

    create mysql-pool, index-webserver and general index

---
+++ www/app/webserver/index.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const express = require("express");
+
+/*
+aqui tenemos que importar el index de routes
+y debajo del json utilizarlas(app.use...)
+*/
+
+const app = express();
+
+app.use(express.json());
+
+let server = null;
+async function listen(port) {
+  if (server) {
+    return server;
+  }
+
+  try {
+    server = await app.listen(port);
+    return server;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
+
+async function close() {
+  if (server) {
+    await server.close();
+    server = null;
+  } else {
+    console.error("Can not close a non started server");
+  }
+}
+
+module.exports = {
+  listen,
+  close
+};
