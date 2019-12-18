aa35d74382a6ccef38b11829665419ab215f9e99
Jandro <jandroportonovo@hotmail.com>
2019-12-18 18:35:25 +0100

    not finished create account controller

---
+++ www/app/webserver/controllers/account/create-account-controller.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const Joi = require('@hapi/joi');
+const mysqlPool = require('../../../database/mysql-pool');
+const sengridMail = require('@sengrid/mail');
+const uuidv4 = require('uuid/v4');
+const bcrypt = require('bcrypt');
+
+sengridMail.setApiKey(proccess.env.SENGRID_API_KEY);
+
+async function validate (payload) {
+    const schema = Joi.object({
+        email : Joi.string().email().required(),
+        password: Joi.string().regex(/^[a-zA-Z0-9]{6,30}$/)
+    });
+
+    Joi.assert(payload, schema);
+}
+
+async function SendWelcomeEmail (email) {
+    const [username,] = email.split('@');
+    const message = {
+        to: email,
+        from: 'techieshangouts@yopmail.com',
+        subject: 'Welcome to TechiesHangouts! :D',
+        text: `Bienvenido a TechiesHangouts, ${username}! Todo nuestro equipo quiere darte la bienvenida, esperemos que disfrutes de grandes experiencias y oportunidades! Nosotros estamos encantados de aportar nuestro granito de arena para que eso suceda `
+        html: `Bienvenido a TechiesHangouts, ${username}! Todo nuestro equipo quiere darte la bienvenida, esperemos que disfrutes de grandes experiencias y oportunidades! Nosotros estamos encantados de aportar nuestro granito de arena para que eso suceda `
+
+    };
+
+    const data = await sengridMail.send(message);
+    return data;
+
+}
+
+async function createAccount(req,res,next){
+    const accountData = { ...req.body };
+
+    try {
+        await validate()
+        
+    } catch (e) {
+        console.error(e);
+        return res.status(400).send(e)
+    }
+}
