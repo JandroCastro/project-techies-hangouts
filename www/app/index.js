d4281be3b85c75659c52a6c61746c8cc01300bf4
[jose] <jose_sera_sera@hotmail.com>
2019-12-20 20:29:32 +0100

    nada

--- www/app/index.js
+++ www/app/index.js
@@ -3,10 +3,7 @@
 require("dotenv").config();
 const mysqlPool = require("./database/mysql-pool");
 const webServer = require("./webserver");
-<<<<<<< HEAD
 
-=======
->>>>>>> eb97fcbb5cb9ceade8e47eef9732effcb012ad8d
 const port = process.env.PORT;
 
 async function initApp() {
