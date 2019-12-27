99d38f9c882d54de86ddbbe094f33cb16a64a01b
[jose] <jose_sera_sera@hotmail.com>
2019-12-27 12:48:40 +0100

    correction syntax

--- www/app/webserver/routes/hangout-router.js
+++ www/app/webserver/routes/hangout-router.js
@@ -4,8 +4,7 @@ const checkUserSession = require("../controllers/user/check-user-session");
 const createHangout = require("../controllers/hangouts/create-hangout-controller");
 const getHangout = require("../controllers/hangouts/get-hangout-controller");
 
-const router = express.router();
+const router = express.Router();
 router.post("/hangouts", checkUserSession, createHangout);
-router.get("/hangouts/:hangoutId", checkUserSession, getHangout);
 
 module.exports = router;
