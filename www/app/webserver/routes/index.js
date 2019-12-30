3ffd77946c2bb930276a83ffcd013a909fafd5b5
Alejandro Castro Alarcón <jandroportonovo@hotmail.com>
2019-12-30 13:17:13 +0100

    corregir comillas en las querys y nombre de archivo de create profile

--- www/app/webserver/routes/index.js
+++ www/app/webserver/routes/index.js
@@ -3,9 +3,11 @@
 const userRouter = require("./user-router");
 const hangoutRouter = require("./hangout-router");
 const authRouter = require("./auth-router");
+const profileRouter = require("./profile-router");
 
 module.exports = {
   userRouter,
   hangoutRouter,
-  authRouter
+  authRouter,
+  profileRouter
 };
