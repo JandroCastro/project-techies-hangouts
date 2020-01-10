c83f632a8aeb0f0f3b67d9197b9c73dc502da8e4
Alejandro Castro Alarcón <jandroportonovo@hotmail.com>
2020-01-10 22:43:18 +0100

    añadir la ruta de obtener todas las hangouts

--- www/app/webserver/routes/hangout-router.js
+++ www/app/webserver/routes/hangout-router.js
@@ -3,8 +3,10 @@ const express = require("express");
 const checkUserSession = require("../controllers/user/check-user-session");
 const createHangout = require("../controllers/hangouts/create-hangout-controller");
 const getHangout = require("../controllers/hangouts/get-hangout-controller");
+const getAllHangouts = require("../controllers/hangouts/get-all-hangouts-controller");
 
 const router = express.Router();
 router.post("/hangouts", checkUserSession, createHangout);
 router.get("/hangouts/:hangoutId", checkUserSession, getHangout);
+router.get("/hangouts", checkUserSession, getAllHangouts);
 module.exports = router;
