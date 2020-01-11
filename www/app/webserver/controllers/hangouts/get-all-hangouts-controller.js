8c73f1cfa23063820c36603f4e1e17ac5438d405
Alejandro Castro Alarcón <jandroportonovo@hotmail.com>
2020-01-11 12:51:55 +0100

    fix get all hangouts controller

--- www/app/webserver/controllers/hangouts/get-all-hangouts-controller.js
+++ www/app/webserver/controllers/hangouts/get-all-hangouts-controller.js
@@ -3,10 +3,9 @@
 const mySqlPool = require("../../../database/mysql-pool");
 
 /**
- * Falla el formato del "const = today" para introducir en la query. Sería muy
- * interesante que hubiese solo una columna en la tabla con DATETIME para
- * poder mostrar
- * los eventos que no han ocurrido ya (por fecha y hora). De esta forma
+ *
+ * Sería muy interesante que hubiese solo una columna en la tabla con DATETIME para
+ * poder mostrar los eventos que no han ocurrido ya (por fecha y hora). De esta forma
  * puede pasar que te muestre eventos ya ocurridos ese día o que no
  * te muestre los eventos del día en que realizas la búsqueda, no lo sé.
  *
@@ -17,15 +16,13 @@ async function getAllHangouts(req, res, next) {
   try {
     connection = await mySqlPool.getConnection();
     const sqlQuery = `SELECT * FROM Events WHERE
-         event_date  = (
-    SELECT MAX(?)
-    FROM Events)
+    event_date  >= ?
+    AND event_date < '2150-01-01'
     ORDER BY event_date ASC`;
 
     const today = new Date().toISOString().substring(0, 10);
-    console.log(today);
 
-    const [rows] = await connection.execute(sqlQuery, today);
+    const [rows] = await connection.execute(sqlQuery, [today]);
     connection.release();
     if (rows.length === 0) {
       return res.status(404).send();
