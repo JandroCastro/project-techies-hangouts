01fc614176930bb17d4bba25c435a1432fb7e6f6
Alejandro Castro Alarcón <jandroportonovo@hotmail.com>
2020-01-11 12:21:04 +0100

    fix controller get one hangout

--- www/app/webserver/controllers/hangouts/get-hangout-controller.js
+++ www/app/webserver/controllers/hangouts/get-hangout-controller.js
@@ -1,37 +1,16 @@
 "use strict";
-const Joi = require("@hapi/joi");
+
 const mySqlPool = require("../../../database/mysql-pool");
-
-async function validate(payload) {
-  const schema = Joi.object({
-    noteId: Joi.string()
-      .guid({
-        version: ["uuidv4"]
-      })
-      .required()
-  });
-
-  Joi.assert(payload, schema);
-}
 
 async function getHangout(req, res, next) {
   const { hangoutId } = req.params;
-  const { userId } = req.claims;
-  try {
-    const payload = {
-      hangoutId
-    };
-    await validate(payload);
-  } catch (e) {
-    return res.status(400).send(e);
-  }
 
   let connection;
   try {
     connection = await mySqlPool.getConnection();
     const sqlQuery = `SELECT * FROM Events WHERE id = ?`;
 
-    const [rows] = await connection.execute(sqlQuery, [hangoutId, userId]);
+    const [rows] = await connection.execute(sqlQuery, [hangoutId]);
     connection.release();
     if (rows.length === 0) {
       return res.status(404).send();
