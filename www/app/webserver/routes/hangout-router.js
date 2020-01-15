55943154ac81eb22a244c08148f089fe8cd61b5a
[jose] <jose_sera_sera@hotmail.com>
2020-01-15 13:07:57 +0100

    create router-hangout-delete

--- www/app/webserver/routes/hangout-router.js
+++ www/app/webserver/routes/hangout-router.js
@@ -5,10 +5,12 @@ const createHangout = require("../controllers/hangouts/create-hangout-controller
 const getHangout = require("../controllers/hangouts/get-hangout-controller");
 const getAllHangouts = require("../controllers/hangouts/get-all-hangouts-controller");
 const getHangoutByCity = require("../controllers/hangouts/get-hangout-by-city-controller");
+const deleteHangout = require("../controllers/hangouts/delete-hangout-controller");
 
 const router = express.Router();
 router.post("/hangouts", checkUserSession, createHangout);
 router.get("/hangouts/:hangoutId", checkUserSession, getHangout);
 router.get("/hangouts", checkUserSession, getAllHangouts);
 router.get("/hangouts/:cityId", checkUserSession, getHangoutByCity);
+router.delete("/hangouts/:hangoutId", checkUserSession, deleteHangout);
 module.exports = router;
