4f8a3661e800511f91cfc7776a68e6ae542375bf
[jose] <jose_sera_sera@hotmail.com>
2020-01-15 17:20:58 +0100

    conect controller get-hangout-by thematic in router-hangout

--- www/app/webserver/routes/hangout-router.js
+++ www/app/webserver/routes/hangout-router.js
@@ -2,15 +2,17 @@
 const express = require("express");
 const checkUserSession = require("../controllers/user/check-user-session");
 const createHangout = require("../controllers/hangouts/create-hangout-controller");
+const deleteHangout = require("../controllers/hangouts/delete-hangout-controller");
 const getHangout = require("../controllers/hangouts/get-hangout-controller");
 const getAllHangouts = require("../controllers/hangouts/get-all-hangouts-controller");
 const getHangoutByCity = require("../controllers/hangouts/get-hangout-by-city-controller");
-const deleteHangout = require("../controllers/hangouts/delete-hangout-controller");
+const getHangoutByThematic = require("../controllers/hangouts/get-hangout-by-thematic-controller");
 
 const router = express.Router();
 router.post("/hangouts", checkUserSession, createHangout);
 router.get("/hangouts/:hangoutId", checkUserSession, getHangout);
 router.get("/hangouts", checkUserSession, getAllHangouts);
 router.get("/hangouts/:cityId", checkUserSession, getHangoutByCity);
+router.get("/hangouts/:thematicId", checkUserSession, getHangoutByThematic);
 router.delete("/hangouts/:hangoutId", checkUserSession, deleteHangout);
 module.exports = router;
