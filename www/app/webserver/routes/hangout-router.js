51e30ac922e55a3d222752fbda11cba0acce0389
Alejandro Castro Alarcón <jandroportonovo@hotmail.com>
2020-01-20 17:03:39 +0100

    conectar get hangouts by filtro

--- www/app/webserver/routes/hangout-router.js
+++ www/app/webserver/routes/hangout-router.js
@@ -5,18 +5,24 @@ const createHangout = require("../controllers/hangouts/create-hangout-controller
 const deleteHangout = require("../controllers/hangouts/delete-hangout-controller");
 const getHangout = require("../controllers/hangouts/get-hangout-controller");
 const getAllHangouts = require("../controllers/hangouts/get-all-hangouts-controller");
-const getHangoutByCity = require("../controllers/hangouts/get-hangout-by-city-controller");
-const getHangoutByThematic = require("../controllers/hangouts/get-hangout-by-thematic-controller");
+/*const getHangoutByCity = require("../controllers/hangouts/get-hangout-by-city-controller");
+const getHangoutByThematic = require("../controllers/hangouts/get-hangout-by-thematic-controller");*/
+const getHangoutsByQuery = require("../controllers/hangouts/get-hangouts-by-query");
 
 const router = express.Router();
 router.post("/hangouts", checkUserSession, createHangout);
-router.get("/hangouts/:hangoutId", checkUserSession, getHangout);
 router.get("/hangouts", checkUserSession, getAllHangouts);
-router.get("/hangouts/:cityName", checkUserSession, getHangoutByCity);
+router.get("/hangouts/:hangoutId", checkUserSession, getHangout);
+router.get(
+  "/hangouts/filter?city=:cityName&thematics=:thematicName&dates=:datesBetween",
+  checkUserSession,
+  getHangoutsByQuery
+);
+/*router.get("/hangouts/:cityName", checkUserSession, getHangoutByCity);
 router.get(
   "/hangouts/thematic/:thematicName",
   checkUserSession,
   getHangoutByThematic
-);
+);*/
 router.delete("/hangouts/:hangoutId", checkUserSession, deleteHangout);
 module.exports = router;
