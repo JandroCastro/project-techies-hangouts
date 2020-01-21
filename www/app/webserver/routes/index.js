88d3004147ca3165d8caa9ebe525a1c1b75cc91c
[jose] <jose_sera_sera@hotmail.com>
2020-01-21 18:02:32 +0100

    nada

--- www/app/webserver/routes/index.js
+++ www/app/webserver/routes/index.js
@@ -5,11 +5,7 @@ const hangoutRouter = require("./hangout-router");
 const authRouter = require("./auth-router");
 const profileRouter = require("./profile-router");
 const ratingsRouter = require("./ratings-router");
-<<<<<<< HEAD
 const auxRouter = require("./aux-router");
-=======
-const attendanceRouter = require("./attendance-router");
->>>>>>> 4fcdb56ec01d4f472ad81eb7e90a7ee633f54b0a
 
 module.exports = {
   userRouter,
@@ -17,9 +13,5 @@ module.exports = {
   authRouter,
   profileRouter,
   ratingsRouter,
-<<<<<<< HEAD
   auxRouter
-=======
-  attendanceRouter
->>>>>>> 4fcdb56ec01d4f472ad81eb7e90a7ee633f54b0a
 };
