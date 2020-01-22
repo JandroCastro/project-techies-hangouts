9c77e51f0218d5b66157b96f2a764294254ca2e8
[jose] <jose_sera_sera@hotmail.com>
2020-01-22 16:14:28 +0100

    move files all thematics and all cities

--- www/app/webserver/index.js
+++ www/app/webserver/index.js
@@ -8,11 +8,7 @@ const {
   authRouter,
   profileRouter,
   ratingsRouter,
-<<<<<<< HEAD
   auxRouter
-=======
-  attendanceRouter
->>>>>>> 4fcdb56ec01d4f472ad81eb7e90a7ee633f54b0a
 } = require("./routes");
 
 const app = express();
@@ -23,11 +19,7 @@ app.use("/api", authRouter);
 app.use("/api", hangoutRouter);
 app.use("/api", profileRouter);
 app.use("/api", ratingsRouter);
-<<<<<<< HEAD
 app.use("/api", auxRouter);
-=======
-app.use("/api", userRouter);
->>>>>>> 4fcdb56ec01d4f472ad81eb7e90a7ee633f54b0a
 
 let server = null;
 async function listen(port) {
