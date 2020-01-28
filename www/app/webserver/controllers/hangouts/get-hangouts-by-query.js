05b3065e9d8b9efd7a5b82240c6021f705fdac95
Alejandro Castro Alarcón <jandroportonovo@hotmail.com>
2020-01-28 18:00:35 +0100

    Acabar por fin el endpoint de filtrado, ya funciona!

--- www/app/webserver/controllers/hangouts/get-hangouts-by-query.js
+++ www/app/webserver/controllers/hangouts/get-hangouts-by-query.js
@@ -2,51 +2,60 @@
 
 const mySqlPool = require("../../../database/mysql-pool");
 
-function filteringPayload(payload) {
-  let sqlQuery = `SELECT * FROM Events WHERE`;
-  /**
-   * En la query, si no entra con city, se encuentra con un AND que la va
-   * a romper. Por tanto, pienso que a cada paso se debería hacer un split,
-   * y si la última posición de sqlQuery es = WHERE, entonces no se añadiría
-   * AND, en caso de que sea distinto, quiere decir que entró en city, modificó
-   * la query y si hay que añadir AND. Vale el caso tanto para thematic
-   * como para dates
-   */
-  if (payload.city !== "TODAS") {
-    sqlQuery += "city_id = ?";
-  } else if (payload.thematics !== "TODAS") {
-    sqlQuery += "AND thematic_id = ?";
-  } else if (payload.dates !== undefined) {
-    sqlQuery += "AND event_date >= ? AND event_date <= ?";
+function payloadToQuery(payload) {
+  const query = "SELECT * FROM Events WHERE";
+
+  const arrObject = [];
+
+  for (const property in payload) {
+    let data = {
+      column: property,
+      value: payload[property],
+      operator: " "
+    };
+
+    if (data.column === "event_date1") {
+      data.operator = ">=";
+      data.column = "event_date";
+    } else if (data.column === "event_date2") {
+      data.operator = "<=";
+      data.column = "event_date";
+    } else {
+      data.operator = "=";
+    }
+
+    arrObject.push(data);
   }
-  return sqlQuery;
+
+  const parsedParams = arrObject
+    .filter(param => !!param.value)
+    .map(({ column, value, operator }) => `${column} ${operator} "${value}"`)
+    .join(" AND ");
+
+  const solution = `${query} ${parsedParams}`;
+
+  return solution;
 }
 
 async function getHangoutsByQuery(req, res, next) {
   const filters = req.query;
   console.log(filters);
 
-  const filtersQuery = filteringPayload(filters);
+  const sqlQuery = payloadToQuery(filters);
+  console.log(sqlQuery);
 
-  const { city, thematics, dates } = filters;
-
-  const initialDate = dates.substring(0, 10);
-  const finalDate = dates.substring(11, 21);
-
-  const connection = mySqlPool.getConnection();
   try {
-    await connection.query(filtersQuery, {
-      city,
-      thematics,
-      initialDate,
-      finalDate
-    });
+    const connection = await mySqlPool.getConnection();
+
+    const [rows] = await connection.query(sqlQuery);
+
+    if (rows.length === 0) res.status(404).send();
+
+    res.send(rows);
   } catch (e) {
     console.error(e);
     res.status(500).send();
   }
-
-  res.send("Conectado");
 }
 
 module.exports = getHangoutsByQuery;
